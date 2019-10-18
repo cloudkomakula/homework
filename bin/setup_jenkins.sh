1163a8317b01c3918bf9d450165f6a8069c28004
cloudkomakula <56711936+cloudkomakula@users.noreply.github.com>
2019-10-18 12:32:48 +0530

    Update setup_jenkins.sh

--- bin/setup_jenkins.sh
+++ bin/setup_jenkins.sh
@@ -7,13 +7,17 @@ if [ "$#" -ne 3 ]; then
     exit 1
 fi
 
-GUID=$1
-REPO=$2
+GUID=8fa5
+REPO=https://master.na311.openshift.opentlc.com
 CLUSTER=$3
 echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cluster ${CLUSTER}"
 
 # Set up Jenkins with sufficient resources
-# TBD
+oc new-project ${GUID}-jenkins --display-name "${GUID} Shared Jenkins"
+oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi --param DISABLE_ADMINISTRATIVE_MONITORS=true
+oc set resources dc jenkins --limits=memory=2Gi,cpu=2 --requests=memory=1Gi,cpu=500m
+
+
 
 # Create custom agent container image with skopeo
 # TBD
